7c730f68bce482801e66a50e41033793c53cfc43
rnddp9320190816 <rnddp93@naver.com>
2022-10-17 00:03:53 +0900

    (feat) SPRING 필터 추가를 통해 utf-8 인코딩

--- recomend/src/main/java/com/simsim/solution/recomend/config/security/SecurityConfig.java
+++ recomend/src/main/java/com/simsim/solution/recomend/config/security/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.filter.CharacterEncodingFilter;
 
 @EnableWebSecurity
 @Configuration
@@ -16,4 +17,5 @@ public class SecurityConfig  {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http.authorizeRequests().antMatchers("/food/recommend/random").permitAll().and().build();
     }
+
 }
